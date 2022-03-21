e4d0d080b534fcd598e315d115bf012adfb4d278
ꕤ karl ꕤ <97465896+KarlXou@users.noreply.github.com>
2022-03-21 10:34:55 -0500

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+// Let's grad the hero div
+const hero = document.querySelector(`hero`);
+
+// let's modify the background
+hero.style.backgroupImage = `url('./assets/sf.jpg')`;
+
+const name = document.querySelector(`.hero h1 span`);
+name.innerHTML = `Your name`;
+
+const greeting = document.querySelector(`.hero h3`);
+greeting.innerHTML = `Something really clever...`;
+const profilePic = querySelector(`.profile-img`);
